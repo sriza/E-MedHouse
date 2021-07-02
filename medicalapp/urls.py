27801f0d1783d3994cc103f6ee4462f269912005
Sunil Shrestha <sunilshresthamail@gmail.com>
2021-07-02 22:36:56 +0545

    adding more template and snippets files

--- medicalapp/urls.py
+++ medicalapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('', views.index),
+    path('doctor/', views.doctor),
+    path('shop/', views.shop),
+    path('login/', views.login),
+    path('signin/', views.signin),
 ]
--- medicalapp/views.py
+++ medicalapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'medicalapp/index.htm')
+
+def doctor(request):
+    return render(request, 'medicalapp/doctor.htm')
+
+def shop(request):
+    return render(request, 'medicalapp/shop.htm')
+
+def login(request):
+    return render(request, 'medicalapp/login.htm')
+
+def signin(request):
+    return render(request, 'medicalapp/signin.htm')
